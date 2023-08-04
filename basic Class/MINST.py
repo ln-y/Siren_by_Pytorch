52761ee3695910894c547fe3607e438d78c91d39
ln-y <134682565+ln-y@users.noreply.github.com>
2023-08-04 22:26:22 +0800

    Add files via upload

---
+++ basic Class/MINST.py
@@ -0,0 +1,17 @@
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+# 通过ToTensor实例将图像数据从PIL类型变换成32位浮点数格式，
+# 并除以255使得所有像素的数值均在0到1之间
+
+def getData():
+    trans = transforms.ToTensor()
+    mnist_train = torchvision.datasets.FashionMNIST(root="../../MINST_handwrite_number_recongnition/data", train=True,
+                                                    transform=trans, download=False)
+    mnist_test = torchvision.datasets.FashionMNIST(root="../../MINST_handwrite_number_recongnition/data", train=False,
+                                                   transform=trans, download=False)
+    return mnist_train,mnist_test
+
+# print(len(mnist_train), len(mnist_test))
+# print(len(mnist_test[0]))
